e03404db98829d75560f052b0bf383be71c212a5
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-25 21:48:24 -0700

    Complete Return Value of break exercise

---
+++ ruby-basics/reading-documentation/break_return_value.rb
@@ -0,0 +1,11 @@
+#Locate documentation for break and determine what value break sets the return value to for the while loop
+
+#break documentation https://docs.ruby-lang.org/en/2.7.0/syntax/control_expressions_rdoc.html#label-break+Statement
+
+# returns the result of the expression it is "breaking" out of or nil if no arguments are passed
+
+result = while true
+  break
+end 
+
+p result 
