c0d19bf38d088b86537f5a6d14f13d829558b915
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-25 19:23:24 -0700

    Complete while Loops exercise

---
+++ ruby-basics/reading-documentation/while_loops.rb
@@ -0,0 +1,5 @@
+#Locate the description of the while loop in ruby documentation
+
+# the while loop executes while a condition is true 
+
+# https://docs.ruby-lang.org/en/2.7.0/syntax/control_expressions_rdoc.html#label-while+Loop
