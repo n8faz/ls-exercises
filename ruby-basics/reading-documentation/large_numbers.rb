5185f32b051d59d4d91fa10747e151211367d548
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-26 15:44:15 -0700

    Complete Large Numbers exercise

---
+++ ruby-basics/reading-documentation/large_numbers.rb
@@ -0,0 +1,5 @@
+# Using Ruby docs, determine how you can write large numbers in a way that makes them easier to read
+
+# https://docs.ruby-lang.org/en/2.7.0/syntax/literals_rdoc.html#label-Numbers
+
+# Place an underscore anywhere in the number to enhance readability for humans
