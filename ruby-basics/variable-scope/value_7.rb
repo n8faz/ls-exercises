d1e4d70ede22e3f1550f1ad4b6d3d49b87f9ce4a
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-26 17:56:08 -0700

    Complete What's My Value (Part 7) exercise

---
+++ ruby-basics/variable-scope/value_7.rb
@@ -0,0 +1,12 @@
+a = 7
+array = [1,2,3]
+
+array.each do |element|
+  a = element
+end
+
+puts a 
+
+# a = 3
+
+#method invocations are different than method definitions and they can access and manipulate outside variables
