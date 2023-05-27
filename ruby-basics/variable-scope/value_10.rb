360a247f14eba684f0a0a21705cc7b020e5e490b
n8faz <119473201+n8faz@users.noreply.github.com>
2023-05-26 18:09:04 -0700

    Complete What's My Value (Part 10) exercise

---
+++ ruby-basics/variable-scope/value_10.rb
@@ -0,0 +1,15 @@
+a = 7
+array = [1, 2, 3]
+
+def my_value(ary)
+  ary.each do |b|
+    a += b
+  end
+end
+
+my_value(array)
+puts a
+
+# error
+# a is not visible inside each method invocation because invocation is inside method definition with a self contained scope 
+
