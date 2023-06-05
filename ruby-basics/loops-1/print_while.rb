6603a717fadbed3ca9b11a78a878f1c6b28630c3
n8faz <119473201+n8faz@users.noreply.github.com>
2023-06-05 16:53:24 -0700

    Complete Print While exercise

---
+++ ruby-basics/loops-1/print_while.rb
@@ -0,0 +1,20 @@
+# Using a while loop, print 5 random numbers between 0 and 99 (inclusive)
+
+numbers = []
+count = 0
+
+while count < 5
+  numbers = rand(0..99)
+  count += 1
+  puts numbers
+end
+
+=begin launch school solution
+
+numbers = []
+
+while numbers.size < 5
+  numbers << rand(100)
+end
+
+puts numbers
