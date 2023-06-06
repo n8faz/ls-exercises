923071180b660f9cfdbab84ecf18265abdd7229c
n8faz <119473201+n8faz@users.noreply.github.com>
2023-06-05 17:30:07 -0700

    Complete Greet Your Friends exercise

---
+++ ruby-basics/loops-1/greet_friends.rb
@@ -0,0 +1,11 @@
+# Given the following array of names, use a for loop to greet each friend individually
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for name in friends 
+  puts "Hello, #{name}!"
+end
+
+# with each method
+# friends.each {|name| puts "Hello, #{name}!"} 
+
