0c32b9c8e2d98f5a69e3af0df2d079ef1b28e392
n8faz <119473201+n8faz@users.noreply.github.com>
2023-06-08 18:13:00 -0700

    Complete Dividing Numbers exercise

--- ruby-basics/user-input/division.rb
+++ ruby-basics/user-input/division.rb
@@ -6,3 +6,54 @@ end
 
 # It returns true if the input string can be converted to an integer and back to a string without loss of information, false otherwise.
 
+numerator = nil
+denominator = nil 
+
+loop do 
+  puts ">> Please enter the numerator: "
+  numerator = gets.chomp 
+  if valid_number?(numerator) == false 
+    puts ">> Invalid input. Only integers are allowed."
+    next
+  end 
+  loop do 
+    puts ">> Please enter the denominator: "
+    denominator = gets.chomp
+    if valid_number?(denominator) == false  
+      puts ">> Invalid input. Only integers are allowed."
+    elsif denominator.to_i == 0
+      puts ">> Invalid input. A denominator of 0 is not allowed."
+    elsif valid_number?(denominator) == true 
+      break 
+    end
+  end
+  break if valid_number?(numerator) && valid_number?(denominator) && denominator != 0 
+end
+
+answer = numerator.to_i / denominator.to_i
+puts "#{numerator} / #{denominator} is #{answer}"
+
+#launch school solution
+
+numerator = nil
+loop do 
+  puts '>> Please enter the numberator: '
+  numerator = gets.chomp
+  break if valid_number?(numerator)
+  puts '>> Invalid input. Only integers are allowed.'
+end
+
+denominator = nil 
+loop do
+  puts '>> Please enter the denominator: '
+  denominator = gets.chomp
+  if denomiator == '0'
+    puts '>> INvalid input. A denominator of 0 is not allowed.'
+  else 
+    break if valid_number?(denominator)
+    puts '>> Invalid input. Only integers are allowed.'
+  end
+end
+
+result = numerator.to_i / denominator.to_i
+puts "#{numerator} / #{denominator} is #{result}"
