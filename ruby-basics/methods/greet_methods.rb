b691039ac2bc458414230fb7057fb7ab3cb7cfe6
n8faz <119473201+n8faz@users.noreply.github.com>
2023-06-09 22:58:02 -0700

    Complete Greeting Through Methods (Part 1) exercise

---
+++ ruby-basics/methods/greet_methods.rb
@@ -0,0 +1,11 @@
+# Write two methods, one that returns the string "Hello" and one that returns the string "World" then print both strings using puts, combining them into one sentence
+
+def hello 
+  "Hello"
+end
+
+def world 
+  "World"
+end
+
+puts "#{hello} #{world}"
