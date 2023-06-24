52150a64057bb2356d599643b84f5c08d12a37c8
n8faz <119473201+n8faz@users.noreply.github.com>
2023-06-23 17:34:35 -0700

    Complete Counting Sheep (Part 3) exercise

---
+++ ruby-basics/return/count_sheep3.rb
@@ -0,0 +1,19 @@
+# What will the following code print?
+
+def count_sheep
+  5.times do |sheep|
+    puts sheep
+    if sheep >= 2
+      return
+    end
+  end
+end
+
+p count_sheep 
+
+=begin 
+0
+1
+2
+nil 
+=end
