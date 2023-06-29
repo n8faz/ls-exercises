82b6483e95eb6e4a12588183e04aad3958652828
n8faz <119473201+n8faz@users.noreply.github.com>
2023-06-28 18:35:21 -0700

    Complete Stoplight (Part 2) exercise

---
+++ ruby-basics/conditionals/stoplight2.rb
@@ -0,0 +1,11 @@
+#Convert stoplight.rb into if statement
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+if stoplight == 'green'
+  puts "Go!"
+elsif stoplight == 'yellow'
+  puts "Slow down!"
+else 
+  puts "Stop!"
+end 
