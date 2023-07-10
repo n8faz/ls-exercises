15d869cad8d8f9c78533bbe56cee7e2805b0ba45
n8faz <119473201+n8faz@users.noreply.github.com>
2023-07-10 15:48:30 -0700

    Complete Stoplight (Part 3) exercise

--- ruby-basics/conditionals/stoplight3.rb
+++ ruby-basics/conditionals/stoplight3.rb
@@ -3,10 +3,7 @@
 stoplight = ['green', 'yellow', 'red'].sample
 
 case stoplight 
-when 'green'
-  puts 'Go!'
-when 'yellow'
-  puts 'Slow down!'
-else 
-  puts 'Stop!'
-end 
+when 'green' then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+else puts 'Stop!'
+end
