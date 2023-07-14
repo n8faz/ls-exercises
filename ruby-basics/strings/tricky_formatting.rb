94767e619385969e48b787dfcd753805aa2ba72d
n8faz <119473201+n8faz@users.noreply.github.com>
2023-07-14 16:13:08 -0700

    Complete Tricky Formatting exercise

---
+++ ruby-basics/strings/tricky_formatting.rb
@@ -0,0 +1,5 @@
+# Capitalize the value of state then print the modified value. Note that state should have the modified value both before and after you print it.
+
+state = 'tExAs'
+state.capitalize! 
+puts state
