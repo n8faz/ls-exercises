4943ed15c40244de5ecf39cd69719b7563b47569
n8faz <119473201+n8faz@users.noreply.github.com>
2023-07-14 16:35:21 -0700

    Complete Are You There? exercise

---
+++ ruby-basics/strings/colors.rb
@@ -0,0 +1,6 @@
+# print true if colors includes the color 'yellow' and print false if it doesn't. Then, print true if colors includes the color 'purple' and print false if it doesn't.
+
+colors = 'blue pink yellow orange'
+
+p colors.include?('yellow')
+p colors.include?('purple')
