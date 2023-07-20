d7303b2aeded4b14983f4ecb2e990e19f48e4d57
n8faz <119473201+n8faz@users.noreply.github.com>
2023-07-19 17:26:58 -0700

    Add Favorite Number (Part 1) prompt

---
+++ ruby-basics/arrays/favorite_number.rb
@@ -0,0 +1,3 @@
+# Group each name with the number following it by placing the pair in their own array. Then create a nested array containing all three groups (You don't need to write any code just alter the value shown)
+
+['Dave', 7, 'Miranda', 3, 'Jason', 11]
