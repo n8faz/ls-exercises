0ff6b299197dbfee33bef9aa9ecea1b8edda9e88
n8faz <119473201+n8faz@users.noreply.github.com>
2023-07-27 17:00:34 -0700

    Fix typo in Low or Nothing exercise prompt

--- ruby-basics/hashes/low_or_nothing.rb
+++ ruby-basics/hashes/low_or_nothing.rb
@@ -1,4 +1,4 @@
-# In the following code, numbers ins't mutated because # select isn't a destructive method. However, there is a destructive version of #select named #select!. Modify the code to use #select! instead of #select
+# In the following code, numbers isn't mutated because # select isn't a destructive method. However, there is a destructive version of #select named #select!. Modify the code to use #select! instead of #select
 
 numbers = {
   high:   100,
