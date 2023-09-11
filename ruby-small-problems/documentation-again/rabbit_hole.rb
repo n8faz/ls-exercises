54f4992fc4a7f93868ee23c9c4ad72400f3e9081
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-11 12:25:07 -0700

    Complete Down the Rabbit Hole exercise

---
+++ ruby-small-problems/documentation-again/rabbit_hole.rb
@@ -0,0 +1,8 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+# Find the documentation for YAML::load_file.
+
+# https://ruby-doc.org/3.2.2/exts/psych/Psych.html#method-c-load_file
+
+# YAML is an alias for psych
