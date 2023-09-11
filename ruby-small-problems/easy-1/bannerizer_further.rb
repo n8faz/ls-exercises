a421ca727e37492d50b8762e49b621e14cfb0e52
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-11 15:49:25 -0700

    Update to include constant for column size

--- ruby-small-problems/easy-1/bannerizer_further.rb
+++ ruby-small-problems/easy-1/bannerizer_further.rb
@@ -1,26 +1,22 @@
 # Further Exploration of Bannerizer exercise
 # Modify this method so it will truncate the message if it will be too wide to fit inside a standard terminal window (80 columns, including the sides of the box). For a real challenge, try word wrapping very long messages so they appear on multiple lines, but still within a box.
 
-def split_text(text)
-  text_array = []
-  number_of_lines = (text.length / 76) + 1
-  number_of_lines.times do
-    split_text = text.slice!(0, 76)
-    if split_text.length == 76
-      text_array.push(split_text)  
-    elsif split_text.length < 76
-      text_array.push((split_text + ' ' * (76 - split_text.length)))
-    end
-  end
-  text_array
-end
+COLUMN_SIZE = 76 # desired amount of columns - 4 to account for ('+-','-+') and ('| ',' |'). 76 is for standard terminal window
 
-def print_text(text)
-  if text.length >= 76
-   split_text(text).each { |str| puts "| #{str} |"} 
+def print_in_box(text)
+  dashes = ''
+  spaces = ''
+  if text.length >= COLUMN_SIZE
+    COLUMN_SIZE.times {spaces << ' '}
+    COLUMN_SIZE.times {dashes << '-'}
   else
-    puts '| ' + text + ' |'
+    text.length.times {spaces << ' '}
+    text.length.times {dashes << '-'}
   end
+
+  print_box_top(dashes, spaces)
+  print_text(text)
+  print_box_bottom(dashes, spaces)
 end
 
 def print_box_top(dashes, spaces)
@@ -33,20 +29,26 @@ def print_box_bottom(dashes, spaces)
   puts '+-' + dashes + '-+'
 end
 
-def print_in_box(text)
-  dashes = ''
-  spaces = ''
-  if text.length >= 76
-    76.times {spaces << ' '}
-    76.times {dashes << '-'}
+def print_text(text)
+  if text.length >= COLUMN_SIZE
+   split_text(text).each { |str| puts "| #{str} |"} 
   else
-    text.length.times {spaces << ' '}
-    text.length.times {dashes << '-'}
+    puts '| ' + text + ' |'
   end
+end
 
-  print_box_top(dashes, spaces)
-  print_text(text)
-  print_box_bottom(dashes, spaces)
+def split_text(text)
+  text_array = []
+  number_of_lines = (text.length / COLUMN_SIZE) + 1
+  number_of_lines.times do
+    split_text = text.slice!(0, COLUMN_SIZE)
+    if split_text.length == COLUMN_SIZE
+      text_array.push(split_text)  
+    elsif split_text.length < COLUMN_SIZE
+      text_array.push((split_text + ' ' * (COLUMN_SIZE - split_text.length)))
+    end
+  end
+  text_array
 end
 
 print_in_box('')
