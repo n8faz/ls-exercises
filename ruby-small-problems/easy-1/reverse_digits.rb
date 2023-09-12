51b9e2e3bf33ce57c5d2e28f2f4de602df6ca02b
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-12 15:41:56 -0700

    Complete Reverse Digits in Number exercise

---
+++ ruby-small-problems/easy-1/reverse_digits.rb
@@ -0,0 +1,19 @@
+# Write a method that takes a positive integer as an argument and returns that number with its digits reversed.
+
+def reversed_number(num)
+  reverse_num = ""
+  num_string = num.to_s
+  numbers = num_string.length
+  while numbers != 0 do
+    reverse_num << num_string[(numbers - 1)]
+    numbers -= 1
+  end
+  reverse_num.to_i
+end
+
+puts reversed_number(12345) == 54321
+puts reversed_number(12213) == 31221
+puts reversed_number(456) == 654
+puts reversed_number(12000) == 21 # No leading zeros in return value!
+puts reversed_number(12003) == 30021
+puts reversed_number(1) == 1
