46f2a8c3117b2ec5b47e89347f99390c72d89b3f
n8faz <119473201+n8faz@users.noreply.github.com>
2023-09-12 14:54:42 -0700

    Complete Further Exploration Right Triangles

--- ruby-small-problems/easy-1/right_triangles.rb
+++ ruby-small-problems/easy-1/right_triangles.rb
@@ -9,6 +9,62 @@ def triangle(num)
   end
 end
 
-triangle(5)
+#triangle(5)
 
-triangle(9)
+#triangle(9)
+
+def triangle_upside_down(num)
+  increment = 0
+  while num != 0 do
+    puts "*" * num + " " * increment
+    increment += 1
+    num -= 1
+  end
+end
+
+#triangle_upside_down(5)
+
+#triangle_upside_down(9)
+
+def which_corner_triangle(num, corner)
+  increment = 0
+  case corner
+  when "bottom right"
+    increment = 1
+    while num != 0 do
+      puts " " * (num - 1) + "*" * increment
+      increment += 1
+      num -= 1
+    end
+  when "bottom left"
+    increment = 1
+    while num != 0
+      puts "*" * increment + " " * (num - 1)
+      increment += 1
+      num -= 1
+    end
+  when "top right"
+    increment = 0
+    while num != 0 do
+      puts " " * increment + "*" * num
+      increment += 1
+      num -= 1
+    end
+  when "top left"
+    increment = 0
+    while num != 0 do
+      puts "*" * num + " " * increment
+      increment += 1
+      num -= 1
+    end
+  end
+end 
+
+which_corner_triangle(5, "bottom right")
+puts
+which_corner_triangle(5, "bottom left")
+puts 
+which_corner_triangle(5, "top right")
+puts
+which_corner_triangle(5, "top left")
+
