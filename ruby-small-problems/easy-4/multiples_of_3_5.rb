5e74a6f19e7e261db6f24e2fa5e8057463976281
n8faz <119473201+n8faz@users.noreply.github.com>
2023-12-18 16:02:12 -0800

    Complete Further Exploration multiples of 3 and 5

--- ruby-small-problems/easy-4/multiples_of_3_5.rb
+++ ruby-small-problems/easy-4/multiples_of_3_5.rb
@@ -12,3 +12,16 @@ p multisum(3) == 3
 p multisum(5) == 8
 p multisum(10) == 33
 p multisum(1000) == 234168
+
+def multisum_with_reduce(number)
+  sum_array = (1..number).to_a
+  sum_array.reduce(0) do |sum, num|
+    sum += num if num % 3 == 0 || num % 5 == 0
+    sum
+  end
+end
+
+p multisum_with_reduce(3)  #== 3
+p multisum_with_reduce(5)  #== 8
+p multisum_with_reduce(10) # == 33
+p multisum_with_reduce(1000) # == 234168
