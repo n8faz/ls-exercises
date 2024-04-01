8550197c4eed363739b79d2ab0e45af79fc3483d
n8faz <119473201+n8faz@users.noreply.github.com>
2024-04-01 12:36:10 -0700

    Add solutions to Multiply All Pairs

--- ruby-small-problems/easy-7/multiply_all_pairs.rb
+++ ruby-small-problems/easy-7/multiply_all_pairs.rb
@@ -11,3 +11,23 @@ def multiply_all_pairs(arr1, arr2)
 end
 
 p multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+
+# launch school solution
+
+def multiply_all_pairs_ls(arr1, arr2)
+  products = []
+  arr1.each do |num1|
+    arr2.each do |num2|
+      products << num1 * num2
+    end
+  end
+  products.sort
+end
+
+p multiply_all_pairs_ls([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+
+# one line solution
+
+def multiply_all_pairs_one_line(arr1, arr2)
+  arr1.product(arr2).map { |num1, num2| num1 * num2 }.sort
+end
