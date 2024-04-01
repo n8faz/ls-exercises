6eb9105cbba305657242f48d285cafc47ca98764
n8faz <119473201+n8faz@users.noreply.github.com>
2024-04-01 15:31:44 -0700

    Complete Palindromic Substrings

---
+++ ruby-small-problems/easy-8/palindromic_substrings.rb
@@ -0,0 +1,34 @@
+
+def leading_substrings(string)
+  substring = ''
+  string.chars.map { |char| substring += char }
+end
+
+def substrings(string)
+  all_substrings = []
+  until string.empty?
+    all_substrings << leading_substrings(string)
+    string.delete_prefix!(string.chr)
+  end
+  all_substrings.flatten
+end
+
+def palindrome?(input)
+  input == input.reverse && input.length > 1
+end
+
+def palindromes(string)
+  substrings(string).select { |substring| palindrome?(substring)}
+end
+
+
+p palindromes('abcd') == []
+p palindromes('madam') == ['madam', 'ada']
+p palindromes('hello-madam-did-madam-goodbye')== [
+  'll', '-madam-', '-madam-did-madam-', 'madam', 'madam-did-madam', 'ada',
+  'adam-did-mada', 'dam-did-mad', 'am-did-ma', 'm-did-m', '-did-', 'did',
+  '-madam-', 'madam', 'ada', 'oo'
+  ]
+ p palindromes('knitting cassettes') == [
+   'nittin', 'itti', 'tt', 'ss', 'settes', 'ette', 'tt'
+   ]
