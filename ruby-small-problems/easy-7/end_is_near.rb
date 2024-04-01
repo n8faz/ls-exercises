5726c55a2a28468413f15cf1b8627a2adaee07b6
n8faz <119473201+n8faz@users.noreply.github.com>
2024-04-01 12:58:46 -0700

    Complete End is Near Further Exploration

--- ruby-small-problems/easy-7/end_is_near.rb
+++ ruby-small-problems/easy-7/end_is_near.rb
@@ -4,3 +4,15 @@ end
 
 p penultimate('last word') == 'last'
 p penultimate('Launch School is great!') == 'is'
+
+# further exploration
+
+def middle(string)
+  word_array = string.split(' ')
+  word_array[(word_array.length / 2)]
+end
+
+p middle('launch school is very great') == 'is'
+p middle('i love pizza') == 'love'
+p middle('one') == 'one'
+p middle('this sentence is very odd')
